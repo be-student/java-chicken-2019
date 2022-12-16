fe63c82f2646b2c23b2c35f1b7d9d93401e6003a
be-student <presentsong@naver.com>
2022-12-16 10:49:43 +0900

    feat: 주문 내역과 최종 결제 금액을 출력하는 기능을 추가한다

--- src/main/java/domain/OrderDto.java
+++ src/main/java/domain/OrderDto.java
@@ -2,4 +2,25 @@ package domain;
 
 public class OrderDto {
 
+    private final String menu;
+    private final int count;
+    private final int money;
+
+    public OrderDto(String menu, int count, int money) {
+        this.menu = menu;
+        this.count = count;
+        this.money = money;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import domain.Menu;
+import domain.OrderDto;
 import domain.Table;
 import java.util.List;
 
@@ -55,5 +56,20 @@ public class OutputView {
 
     public static void printErrorMessage(String errorMessage) {
         System.out.println(ERROR_PREFIX + errorMessage);
+    }
+
+    public static void printOrder(List<OrderDto> orders) {
+        System.out.println();
+        System.out.println("## 주문 내역");
+        System.out.println("메뉴 수량 금액");
+        for (OrderDto order : orders) {
+            System.out.println(order.getMenu() + " " + order.getCount() + " " + order.getMoney());
+        }
+    }
+
+    public static void printTotalMoney(int totalMoney) {
+        System.out.println();
+        System.out.println("## 최종 결제할 금액");
+        System.out.println(totalMoney + "원");
     }
 }
