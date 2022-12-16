7bb601b569fb0e30824ee94709134c8c47d010d6
be-student <presentsong@naver.com>
2022-12-16 10:57:12 +0900

    feat: equals, hashcode를 새롭게 작성한다

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,16 +1,36 @@
 package domain;
 
+import java.util.Objects;
+
 public class Menu {
+
     private final int number;
     private final String name;
     private final Category category;
     private final int price;
 
-    public Menu(final int number, final String name, final Category category, final int price) {
+    public Menu(int number, String name, Category category, int price) {
         this.number = number;
         this.name = name;
         this.category = category;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Menu menu = (Menu) o;
+        return number == menu.number && price == menu.price && name.equals(menu.name) && category == menu.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name, category, price);
     }
 
     @Override
