132a6c5b22e17a22062bae3e14d42eb45fede846
be-student <presentsong@naver.com>
2022-12-16 09:56:43 +0900

    feat: 테이블에 결제 정보에 대한 내용을 추가한다

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,14 +1,28 @@
 package domain;
 
 public class Table {
-    private final int number;
 
-    public Table(final int number) {
+    private final int number;
+    private boolean ordered = false;
+
+    public Table(int number) {
         this.number = number;
     }
 
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    public void ordered() {
+        ordered = true;
+    }
+
+    public void payed() {
+        ordered = false;
+    }
+
+    public boolean isOrdered() {
+        return ordered;
     }
 }
